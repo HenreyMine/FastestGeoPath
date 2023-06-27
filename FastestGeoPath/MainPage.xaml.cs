543f0295a1736b3758a0e8e7a40f406b61c283bf
Hnerey <danilshmyrin@mail.ru>
2023-06-27 20:47:35 +0400

    Added geo location.

--- FastestGeoPath/MainPage.xaml.cs
+++ FastestGeoPath/MainPage.xaml.cs
@@ -9,6 +9,11 @@ public partial class MainPage : ContentPage
     readonly string textStart = "Press button to start tracking path";
     readonly string textStop = "Press button to stop tracking path";
 
+    private CancellationTokenSource _cancelTokenSource;
+    private bool _isCheckingLocation;
+
+    readonly IDispatcherTimer locationTimer;
+
     public MainPage()
     {
         timer = Dispatcher.CreateTimer();
@@ -20,10 +25,14 @@ public partial class MainPage : ContentPage
             UpdateTimerOnScreen();
         };
 
+        locationTimer = Dispatcher.CreateTimer();
+        locationTimer.Interval = TimeSpan.FromSeconds(10);
+        locationTimer.Tick += async (s, e) => await GetCurrentLocation();
+
         InitializeComponent();
     }
 
-    private void OnTrackingClicked(object sender, EventArgs e)
+    private async void OnTrackingClicked(object sender, EventArgs e)
     {
         if (!isTimerGoing)
         {
@@ -33,12 +42,16 @@ public partial class MainPage : ContentPage
 
             UpdateTimerOnScreen();
 
+            locationTimer.Start();
+            await GetCurrentLocation();
+
             InstructionLabel.Text = textStop;
             SemanticScreenReader.Announce(InstructionLabel.Text);
         }
         else
         {
             timer.Stop();
+            locationTimer.Stop();
             isTimerGoing = false;
             InstructionLabel.Text = textStart;
             SemanticScreenReader.Announce(InstructionLabel.Text);
@@ -49,6 +62,44 @@ public partial class MainPage : ContentPage
     {
         TimerLabel.Text = timerTime.ToString();
         SemanticScreenReader.Announce(TimerLabel.Text);
+    }
+
+    public async Task GetCurrentLocation()
+    {
+        try
+        {
+            _isCheckingLocation = true;
+
+            GeolocationRequest request = new(GeolocationAccuracy.High, TimeSpan.FromSeconds(10));
+
+            _cancelTokenSource = new CancellationTokenSource();
+
+            Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+            if (location != null)
+            {
+                LocationLabel.Text = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+                SemanticScreenReader.Announce(LocationLabel.Text);
+            }
+        }
+        // Catch one of the following exceptions:
+        //   FeatureNotSupportedException
+        //   FeatureNotEnabledException
+        //   PermissionException
+        catch (Exception ex)
+        {
+            // Unable to get location
+        }
+        finally
+        {
+            _isCheckingLocation = false;
+        }
+    }
+
+    public void CancelRequest()
+    {
+        if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+            _cancelTokenSource.Cancel();
     }
 }
 
